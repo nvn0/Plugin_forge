92e10f7305eb4d28323e2d7bf3c62327fe6b71c0
nuno <75589883+nvn0@users.noreply.github.com>
2024-05-27 20:15:18 +0100

    ajustes nos comentários do codigo

--- PortController/Program.cs
+++ PortController/Program.cs
@@ -26,23 +26,23 @@ namespace PortController
                 File.Delete(socketPath);
             }
             
-            // Criar o socket Unix
+            // Criar a unix socket
             Socket serverSocket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified);
 
             try
             {
-                // ligar o socket à unix socket
+                // ligar o socket a unix socket
                 serverSocket.Bind(new UnixDomainSocketEndPoint(socketPath));
 
-                // Definir o tamanho máximo da fila de conexão pendentes
+                // Definir o tamanho máximo da fila de conexoes pendentes
                 serverSocket.Listen(5);
 
-                Console.WriteLine("À espera de pedidos...");
+                Console.WriteLine("A espera de pedidos...");
 
-                // Loop para aceitar conexões
+                // Loop para aceitar conexoes
                 while (true)
                 {
-                    // Aceitar a conexão do cliente
+                    // Aceitar a conexao do cliente
                     Socket clientSocket = serverSocket.Accept();
 
                     SocketData data = new SocketData();
@@ -50,7 +50,7 @@ namespace PortController
                     Thread thread = new Thread(() => data.ReceberJson(clientSocket));
                     thread.Start();
 
-                    //data.ReceberJson(clientSocket);
+                    
 
                 }
 
@@ -66,111 +66,8 @@ namespace PortController
             }
 
 
-
-
         }
 
-
-        //------------------------------------- Executar comandos--------------------------------
-
-
-        /*
-        static void execRegra()
-        {
-
-            Firewall firewall = new Firewall();
-
-            Thread thread1 = new Thread(firewall.AllowSshIn);
-
-            thread1.Start();
-
-
-
-        }
-
-
-        public static void execRegraContainer(dynamic name, dynamic type, dynamic action, dynamic port)
-        {
-
-            string opcao = type;
-
-            switch (opcao)
-            {
-                case "lxc":
-                    Console.WriteLine("Opção 1 selecionada.");
-
-                    Lxc lxc = new Lxc();
-
-                    //lxc.OpenPort(name, port);
-                    lxc.ApiCommand(name);
-
-
-                    break;
-                case "incus":
-                    Console.WriteLine("Opção 2 selecionada.");
-                    break;
-                case "docker":
-                    Console.WriteLine("Opção 3 selecionada.");
-                    break;
-                default:
-                    Console.WriteLine("Opção inválida. (execRegraContainer)");
-                    break;
-            }
-
-
-        }
-
-
-        public static string[] GetInfoPortsContainer(dynamic name, dynamic type)
-        {
-
-            string opcao = type;
-
-            switch (opcao)
-            {
-                case "lxc":
-                    Console.WriteLine("Opção 1 selecionada. (getInfo)");
-
-                    Lxc lxc = new Lxc{ Nome = name, Tipo = type };
-
-                    //Thread thread1 = new Thread(() => lxc.GetInfo(name));
-
-                    //return 
-
-                    //Console.WriteLine(lxc.GetInfo(name));
-
-                    //Console.WriteLine(lxc.GetPorts());
-                    Console.WriteLine(lxc.GetPorts2());
-                    string[] array = lxc.GetPorts2().ToArray();
-
-                    return array;
-
-                    
-
-                    break;
-                    
-                case "incus":
-                    Console.WriteLine("Opção 2 selecionada.");
-                    string[] array2 = { "1" };
-                    return array2;
-
-                    break;
-                case "docker":
-                    Console.WriteLine("Opção 3 selecionada.");
-
-                    string[] array3 = { "1" };
-                    return array3;
-                    break;
-                default:
-                    Console.WriteLine("Opção inválida. (GetInfoPortsContainer)");
-                    string[] array4 = { "1" };
-                    return array4;
-                    break;
-                    
-            }
-
-        
-        }
-        */
+       
     }
 }
